f3343d5c358e0ff25f969a26d33b9a1673a3d805
soufianeRabie <rabyasoufiane@gmail.com>
2024-07-18 22:50:32 +0100

    fix import name

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -5,9 +5,6 @@ const Profile = ({name , desc , data ,
   handleEdit , handleDelete 
 }) => {
 
-
-  console.log('newjs');
-
   return (
     <section className='w-full'>
       <h1 className='head_text text-left '>
